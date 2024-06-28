aca73491b733c2507aa0101c2e695829964aa789
ravinpradhitya <ravinpradhitya@yahoo.com>
2024-06-28 23:29:38 +0700

    first commit

--- app/src/main/java/com/example/starbucksappclone/onboarding/LoginScreen.kt
+++ app/src/main/java/com/example/starbucksappclone/onboarding/LoginScreen.kt
@@ -1,6 +1,5 @@
 package com.example.starbucksappclone.onboarding
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -12,13 +11,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Checkbox
-import androidx.compose.material.CheckboxDefaults
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Switch
-import androidx.compose.material.SwitchDefaults
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -28,18 +23,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import com.example.starbucksappclone.R
 import com.example.starbucksappclone.navigation.Screens
 import com.example.starbucksappclone.screens.FloatingButton
-import com.example.starbucksappclone.screens.LoginTextFieldWithDropDown
 import com.example.starbucksappclone.screens.LoginTextFieldWithIcon
 
 @Composable
